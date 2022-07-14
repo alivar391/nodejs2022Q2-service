8b728d03c5c12d591a34a5fffed8bc08ca790534
alivar391 <patriot391@mail.ru>
2022-07-14 22:45:11 +0300

    feat: add swagger, yaml

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { SwaggerModule } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
+import { dirname, join } from 'path';
+import { parse } from 'yaml';
 import { AppModule } from './app.module';
+import { readFile } from 'fs/promises';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const rootDirname = dirname(__dirname);
+  const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
+  const document = parse(DOC_API);
+
+  SwaggerModule.setup('doc', app, document);
   await app.listen(4000);
 }
+
 bootstrap();
