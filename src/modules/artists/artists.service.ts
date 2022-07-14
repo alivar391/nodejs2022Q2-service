da6d1d4e523d27e16692d52c20136b8260cd5799
alivar391 <patriot391@mail.ru>
2022-07-15 00:01:46 +0300

    feat: edit service

--- src/modules/artists/artists.service.ts
+++ src/modules/artists/artists.service.ts
@@ -2,28 +2,34 @@ import { Injectable } from '@nestjs/common';
 import { ArtistsDataBase } from './artists-storage';
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
+import { v4 as uuidv4 } from 'uuid';
+import { Artist } from './entities/artist.entity';
 
 @Injectable()
 export class ArtistsService {
   constructor(private readonly artistsDataBase: ArtistsDataBase) {}
 
-  create(createArtistDto: CreateArtistDto) {
-    return 'This action adds a new artist';
+  create(createArtistDto: CreateArtistDto): Artist {
+    const newArtist = {
+      ...createArtistDto,
+      id: uuidv4(),
+    };
+    return this.artistsDataBase.create(newArtist);
   }
 
   findAll() {
-    return `This action returns all artists`;
+    return this.artistsDataBase.findAll();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} artist`;
+  findOne(id: string) {
+    return this.artistsDataBase.findOne(id);
   }
 
-  update(id: number, updateArtistDto: UpdateArtistDto) {
-    return `This action updates a #${id} artist`;
+  update(id: string, updateArtistDto: UpdateArtistDto) {
+    return this.artistsDataBase.update(id, updateArtistDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} artist`;
+  remove(id: string) {
+    return this.artistsDataBase.delete(id);
   }
 }
