71241e702dac00c817cf155a74244c94e9cbb136
alivar391 <patriot391@mail.ru>
2022-07-15 14:15:59 +0300

    feat: fill track dto, entity

--- src/modules/tracks/dto/create-track.dto.ts
+++ src/modules/tracks/dto/create-track.dto.ts
@@ -1,1 +1,25 @@
-export class CreateTrackDto {}
+import {
+  IsString,
+  IsNotEmpty,
+  IsUUID,
+  IsOptional,
+  IsInt,
+} from 'class-validator';
+
+export class CreateTrackDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsUUID()
+  @IsOptional()
+  artistId: string | null; // refers to Artist
+
+  @IsUUID()
+  @IsOptional()
+  albumId: string | null; // refers to Album
+
+  @IsInt()
+  @IsNotEmpty()
+  duration: number; // integer number
+}
