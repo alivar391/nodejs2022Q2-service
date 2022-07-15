a80890aff4a770748cf6c01ef06f465073547a72
alivar391 <patriot391@mail.ru>
2022-07-15 14:19:33 +0300

    feat: add trackDataBase

---
+++ src/modules/tracks/tracks-storage.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { UpdateTrackDto } from './dto/update-track.dto';
+import { Track } from './entities/track.entity';
+
+@Injectable()
+export class TracksDataBase {
+  tracks: Track[] = [];
+
+  create(track: Track) {
+    this.tracks.push(track);
+    return track;
+  }
+
+  findAll() {
+    return this.tracks;
+  }
+
+  findOne(id: string) {
+    return this.tracks.find((track) => track.id === id);
+  }
+
+  update(id: string, newTrack: UpdateTrackDto): Track {
+    const track = this.tracks.find((track) => track.id === id);
+    Object.assign(track, newTrack);
+    return track;
+  }
+
+  delete(id: string) {
+    const length = this.tracks.length;
+    this.tracks = this.tracks.filter((item) => item.id !== id);
+    return length === this.tracks.length;
+  }
+}
