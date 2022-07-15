b052c7c64d371bb946ce88ba2d6ece8b57982862
alivar391 <patriot391@mail.ru>
2022-07-15 12:40:54 +0300

    feat: implement dto album

---
+++ src/modules/albums/albums-storage.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { UpdateArtistDto } from './dto/update-artist.dto';
+import { Artist } from './entities/artist.entity';
+
+@Injectable()
+export class ArtistsDataBase {
+  artists: Artist[] = [];
+
+  create(artist: Artist) {
+    this.artists.push(artist);
+    return artist;
+  }
+
+  findAll() {
+    return this.artists;
+  }
+
+  findOne(id: string) {
+    return this.artists.find((artist) => artist.id === id);
+  }
+
+  update(id: string, newArtist: UpdateArtistDto): Artist {
+    const artist = this.artists.find((artist) => artist.id === id);
+    Object.assign(artist, newArtist);
+    return artist;
+  }
+
+  delete(id: string) {
+    const length = this.artists.length;
+    this.artists = this.artists.filter((item) => item.id !== id);
+    return length === this.artists.length;
+  }
+}
--- src/modules/albums/dto/create-album.dto.ts
+++ src/modules/albums/dto/create-album.dto.ts
@@ -1,1 +1,15 @@
-export class CreateAlbumDto {}
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateAlbumDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsInt()
+  @IsNotEmpty()
+  year: number;
+
+  @IsString()
+  @IsNotEmpty()
+  artistId: string | null;
+}
