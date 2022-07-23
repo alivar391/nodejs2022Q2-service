101914194704758b437d279143da7407e6be19b7
alivar391 <patriot391@mail.ru>
2022-07-23 12:15:08 +0300

    refactor: refactor artist service

--- src/modules/artists/artists.service.ts
+++ src/modules/artists/artists.service.ts
@@ -1,31 +1,26 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { ArtistsDataBase } from './artists-storage';
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
-// import { v4 as uuidv4 } from 'uuid';
-// import { Artist } from './entities/artist.entity';
-import { AlbumsDataBase } from '../albums/albums-storage';
-import { TracksDataBase } from '../tracks/tracks-storage';
-import { FavoritesDataBase } from '../favorites/favorites-storage';
+import { Artist } from './entities/artist.entity';
 import { PrismaService } from '../../prisma.service';
 
 @Injectable()
 export class ArtistsService {
   constructor(private prisma: PrismaService) {}
 
-  async create(createArtistDto: CreateArtistDto) {
+  async create(createArtistDto: CreateArtistDto): Promise<Artist> {
     const res = await this.prisma.artist.create({
       data: createArtistDto,
     });
     return res;
   }
 
-  async findAll() {
+  async findAll(): Promise<Artist[]> {
     const res = await this.prisma.artist.findMany();
     return res;
   }
 
-  async findOne(id: string) {
+  async findOne(id: string): Promise<Artist> {
     const artist = await this.prisma.artist.findUnique({ where: { id } });
     if (!artist) {
       throw new NotFoundException('Not found');
@@ -33,7 +28,7 @@ export class ArtistsService {
     return artist;
   }
 
-  async update(id: string, updateArtistDto: UpdateArtistDto) {
+  async update(id: string, updateArtistDto: UpdateArtistDto): Promise<Artist> {
     const artist = await this.prisma.artist.findUnique({ where: { id } });
     if (!artist) {
       throw new NotFoundException('Not found');
@@ -44,12 +39,12 @@ export class ArtistsService {
     });
   }
 
-  async remove(id: string) {
+  async remove(id: string): Promise<void> {
     const artist = await this.prisma.artist.findUnique({ where: { id } });
     if (!artist) {
       throw new NotFoundException('Not found');
     }
-    return this.prisma.artist.delete({ where: { id } });
+    this.prisma.artist.delete({ where: { id } });
 
     // this.albumsDataBase
     //   .findAll()
