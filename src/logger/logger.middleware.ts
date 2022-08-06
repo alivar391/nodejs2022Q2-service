f06e6bec5a89bbca5ef7f2e9d7207c23fcab37c1
alivar391 <patriot391@mail.ru>
2022-08-07 02:38:57 +0300

    feat: add stringify to body and query

--- src/logger/logger.middleware.ts
+++ src/logger/logger.middleware.ts
@@ -6,9 +6,10 @@ export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     const { url, query, body } = req;
     const { statusCode } = res;
-
+    const bodyStringify = JSON.stringify(body);
+    const queryStringify = JSON.stringify(query);
     console.log(
-      `URL: ${url}, Status code: ${statusCode}, Body: ${body}, Query params: ${query}`,
+      `URL: ${url}, Status code: ${statusCode}, Body: ${bodyStringify}, Query params: ${queryStringify}`,
     );
 
     next();
